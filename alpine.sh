2a2f9bdb69b3d009118560778169b9c3b29a6719
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2021-09-03 14:25:55 +0200

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,23 +1,34 @@
+USERNAME=kaarmu
 UPDATE=apk update && apk upgrade
 INSTALL=apk add
 
-$UPDATE
-$INSTALL openssh git curl neovim 
+# Go to home and go into super-user mode
+cd
+su
 
+# Install basics
+$UPDATE
+$INSTALL curl openssh git neovim 
+
+# the normal directories
 mkdir -p ~/.config ~/.local/{share,bin} ~/.cache
 PATH=PATH:~/.local/bin
 
+# must fix sudo
+$INSTALL sudo
+addgroup $USERNAME wheel
+EDITOR=nvim visudo
+
+# change to zsh
+$INSTALL zsh shadow
+chsh -s "$(which zsh)" 
+
+# startship prompt
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# dotfiles
 git clone https://github.com/TheLocehiliosan/yadm.git ~/.local/share/yadm
 ln -s ~/.local/share/yadm/yadm ~/.local/bin/yadm
 
-if [ which chsh ] then
-  $INSTALL zsh
-  chsh -s $(which zsh)
-  # random dependencies
-  $INSTALL ncurses
-  $INSTALL zsh-vcs # for alpine
-  # git clone https://github.com/zdharma/zinit.git ~/.zinit/bin # change this to XDG later; use ZINIT[HOME_DIR]
-else
-  echo 'Could not install zsh'
-fi
-
+# exit su mode and go back to normal mode
+exit 
