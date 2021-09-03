8910d760b66a1a313d8b327f98f10a23bd4ba219
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2021-09-04 01:56:46 +0200

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,28 +1,26 @@
-#! /bin/sh                                                                   
-NAME="kaarmu"                                                                
-UPDATE="apk update && apk upgrade"                                           
-INSTALL="apk add"                                                            
-BASIC_PKGS="curl openssh git neovim nano"                                    
+#! /bin/sh
+NAME="kaarmu"
+UPDATE="apk update && apk upgrade"
+INSTALL="apk add"
+BASIC_PKGS="curl openssh git neovim nano"
 
 # the normal directories
 mkdir -p ~/.config ~/.local/{share,bin} ~/.cache
-PATH=$PATH:~/.local/bin
 
 # Install basics
 read -p "Press <enter> to install \"$BASIC_PKGS\""
 su -c "$UPDATE && $INSTALL $BASIC_PKGS"
 
 # must fix sudo
-su -c $INSTALL sudo && addgroup $NAME wheel
-read -p "Press <enter> to update sudoers file"
-EDITOR=nano visudo
+read -p "Press <enter> to install and configure \"sudo\""
+su -c "$INSTALL sudo && addgroup $NAME wheel && EDITOR=nano visudo"
 
 # change to zsh
-sudo $INSTALL zsh shadow
-sudo chsh -s "$(which zsh)"
+read -p "Press <enter> to install \"zsh starship chezmoi\""
+su -c "$INSTALL zsh shadow && chsh -s $(which zsh)"
 
 # startship prompt
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
-# dotfiles
+# dotfiles management
 sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply kaarmu
