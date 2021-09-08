a49296e417345ec9b98a9aff59e116c59483f8ce
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2021-09-08 20:21:31 +0200

    Add files via upload

---
+++ debian.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+NAME="kaarmu"
+BASIC_PKGS="curl openssh-client git"
+
+# the normal directories
+mkdir -p ~/.config ~/.local/{share,bin} ~/.cache
+
+# Install basics
+read -p "Press <enter> to install \"$BASIC_PKGS\""
+sudo apt update
+sudo apt upgrade
+sudo install "$BASIC_PKGS"
+
+# change to zsh
+read -p "Press <enter> to install \"zsh starship chezmoi\""
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# startship prompt
+sudo curl -fsSL https://starship.rs/install.sh
+
+# dotfiles management
+sudo curl -fsLS git.io/chezmoi -- init --apply kaarmu"
